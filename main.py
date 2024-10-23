903d5121cf32359f1f1541e494e9ac2b782aacb0
Gabriel Melgaco <gabrielmelgacom@gmail.com>
2024-10-23 11:01:57 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from apscheduler.schedulers.background import BackgroundScheduler
+import requests
+
+
+app = Flask(__name__)
+
+def ping_site():
+    try:
+        requests.get("https://gerenciamentojaguar.onrender.com")
+        requests.get("https://pedrorochaconsultoria.onrender.com")
+        print('Request Sent')
+    except:
+        pass
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(ping_site, 'interval', minutes=1)
+scheduler.start()
+
+if __name__ == '__main__':
+    app.run(debug=True)
