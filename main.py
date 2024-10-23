c1d2d101ddb5183e3086ae98ef011957d75330f9
Gabriel Melgaco <gabrielmelgacom@gmail.com>
2024-10-23 17:08:22 -0300

    Alterações ChatGPT

--- main.py
+++ main.py
@@ -2,24 +2,28 @@ from flask import Flask
 from apscheduler.schedulers.background import BackgroundScheduler
 import requests
 
-
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return 'Hello, World!'
 
-
 def ping_site():
-    try:
-        requests.get("https://gerenciamentojaguar.onrender.com")
-        requests.get("https://pedrorochaconsultoria.onrender.com")
-        print('Request Sent')
-    except Exception as e:
-        print(f'Error: {e}')
+    urls = ["https://gerenciamentojaguar.onrender.com", "https://pedrorochaconsultoria.onrender.com"]
+    headers = {'User-Agent': 'Mozilla/5.0'}  # Adicionando cabeçalhos para simular uma requisição legítima
+
+    for url in urls:
+        try:
+            response = requests.get(url, headers=headers)
+            if response.status_code == 200:
+                print(f'Request successful for {url}')
+            else:
+                print(f'Failed request for {url}, Status code: {response.status_code}')
+        except Exception as e:
+            print(f'Error pinging {url}: {e}')
 
 scheduler = BackgroundScheduler()
-scheduler.add_job(ping_site, 'interval', minutes=5)
+scheduler.add_job(ping_site, 'interval', minutes=3)  # Ajustar o intervalo conforme necessário
 scheduler.start()
 
 if __name__ == '__main__':
